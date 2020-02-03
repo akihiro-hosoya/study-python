ca6b018bc552fe05011e509e180e3abc38c4b31c
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-03 23:18:29 +0900

    test

--- python-introducton/lesson4.py
+++ python-introducton/lesson4.py
@@ -2,3 +2,18 @@
 
 a = 1
 b = 2
+print(a + b)
+
+for i in [1,2,3,4,5]:
+    print(i)
+print('こんにちは')
+for i in range(1,100+1):
+    print(i)
+print('こんにちは')
+for i in range(0,100):
+    print(i + 1)
+print('こんにちは')
+for i in range(100): # 省略形 for 変数名 in range(繰り返す回数)：
+    print(i + 1)
+for b in "Hello":
+    print(b)
