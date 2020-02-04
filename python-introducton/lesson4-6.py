f7dffce55e176924b422c9ee1ffa52101f61974b
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-04 13:22:25 +0900

    test

--- python-introducton/lesson4-6.py
+++ python-introducton/lesson4-6.py
@@ -1,10 +1,21 @@
 #coding:utf-8
 
-a = "abc"
+a = "abc" #グローバルスコープ
 
 def test():
+    a = 'def' #testローカルスコープ
     print(a)
     return
 
 test()
 print(a)
+
+
+def test():
+    global a #global変数を使える
+    a = 'def'
+    print(a) #a='def'
+    return
+
+test() # def
+print(a) # def
