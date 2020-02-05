6ce9c331d7bce02a2fba3be2c1298425c9e54f40
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-05 19:58:26 +0900

    test

---
+++ python-introducton/lesson6-2.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+
+import tkinter as tk
+root = tk.Tk() #tkinterでウィンドウを表示するときの決まり文句
+root.geometry('400x150') #ウィンドウサイズを変更する
+root.title('数当てゲーム')
+root.mainloop() #tkinterでウィンドウを表示するときの決まり文句
