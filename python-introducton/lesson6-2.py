cb373566ef64560eec12b22eb3e71cdd2df214e0
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-05 22:37:47 +0900

    テスト

--- python-introducton/lesson6-2.py
+++ python-introducton/lesson6-2.py
@@ -1,7 +1,28 @@
 #coding:utf-8
 
 import tkinter as tk
+import tkinter.messagebox as tmsg
+
+#ボタンがクリックされたときに実行される関数を作成する
+def ButtonClick():
+    tmsg.showinfo('テスト', 'クリックされたよ')
+
 root = tk.Tk() #tkinterでウィンドウを表示するときの決まり文句
+#geometryメソッド
 root.geometry('400x150') #ウィンドウサイズを変更する
+#titleメソッド
 root.title('数当てゲーム')
+
+#Labelメソッド
+label1 = tk.Label(root, text='数を入力してね', font=('Helvetica', 14)) 
+#placeメソッド
+label1.place(x = 20, y = 20) 
+#Entryメソッド
+editbox1 = tk.Entry(width = 4, font=('Helvetica', 28))
+editbox1.place(x=120, y=60)
+
+#Buttonメソッド
+button1 = tk.Button(root, text ='チェック', font=('Helvetica', 14), command=ButtonClick)
+button1.place(x=220, y=60)
+
 root.mainloop() #tkinterでウィンドウを表示するときの決まり文句
