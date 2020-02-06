42b5f16d1a3e6fddb28ff47d07d67b875cdfe7ee
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-06 22:21:05 +0900

    test

--- python-introducton/lesson7-9.py
+++ python-introducton/lesson7-9.py
@@ -12,13 +12,13 @@ class Ball:
     #円を動かすためのメソッド
     def move(self, canvas):
         # 今の円を消す
-        canvas.create_oval(self.x-21, self.y-21, self.x+21, self.y+21, fill='white', width=0)
+        self.erase(canvas)
         # x座標を動かす
         self.x = self.x + self.dx
         # y座標を動かす
         self.y = self.y + self.dy
         # 次の位置に円を描く
-        canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, fill=self.color, width=0)
+        self.draw(canvas)
         # 端を超えたら反対向きにする
         if self.x >= canvas.winfo_width():
             self.dx = -1
@@ -28,12 +28,31 @@ class Ball:
             self.dy = -1
         if self.y <= 0:
             self.dy = +1
+    def erase(self, canvas):
+        canvas.create_oval(self.x-21, self.y-21, self.x+21, self.y+21, fill='white', width=0)
+    def draw(self, canvas):
+        canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, fill=self.color, width=0)
 
-# 円を作成
+class Rectangle(Ball):
+    def erase(self, canvas):
+        canvas.create_rectangle(self.x-21, self.y-21, self.x+21, self.y+21, fill='white', width=0)
+    def draw(self, canvas):
+        canvas.create_rectangle(self.x-20, self.y-20, self.x+20, self.y+20, fill=self.color, width=0)
+
+class Triangle(Ball):
+    def erase(self, canvas):
+        canvas.create_polygon(self.x+1, self.y-21, self.x+21, self.y+21, self.x-21, self.y+21, fill='white', width=0)
+    def draw(self, canvas):
+        canvas.create_polygon(self.x, self.y-20, self.x+20, self.y+20, self.x-20, self.y+20, fill=self.color, width=0)
+
+# 図形を作成
 balls = [
-    Ball(400, 300, 1, 1, 'red'),
-    Ball(200, 100, -1, 1, 'green'),
-    Ball(100, 200, 1, -1, 'blue')
+    # 円を作成
+    Ball(500, 300, 1, 1, 'red'),
+    # 四角形を作成
+    Rectangle(400, 300, -1, 1, 'red'),
+    # 三角形を作成
+    Triangle(300, 200, 1, -1, 'red')
 ]
 
 def loop():
@@ -48,7 +67,7 @@ root = tk.Tk()
 root.geometry('800x600')
 
 # キャンバスを置く
-canvas = tk.Canvas(root, width=600, height=400, bg="white")
+canvas = tk.Canvas(root, width=800, height=600, bg="white")
 canvas.place(x=0, y=0)
 
 # タイマーを設定する
