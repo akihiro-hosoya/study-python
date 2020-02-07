8af90b97f3959a0c451bd3690d09205b2d37b10d
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-07 23:15:56 +0900

    test

--- python-izm/basic1.py
+++ python-izm/basic1.py
@@ -1,3 +1,4 @@
+# 文字列
 print('python-izm')
 print("python-izm")
 test_str = """
@@ -6,6 +7,7 @@ test_2
 """
 print(test_str)
 
+# 連結
 test_str = 'python'
 test_str = test_str + '-'
 test_str = test_str + 'izm'
@@ -20,15 +22,19 @@ print(test_str)
 test_str = '012'*3
 print(test_str)
 
+# 文字列へ変換
 test_integer = 100
 print(str(test_integer) + '円')
 
+# 連結
 test_str = 'pyrhon-izm'
 print(test_str.replace('izm', 'izm'))
 
+# 分割
 test_str = 'python-izm'
 print(test_str.split('-'))
 
+# 桁揃え
 test_str = '1234'
 print(test_str.rjust(10, '0'))
 print(test_str.rjust(10, '!'))
@@ -36,6 +42,7 @@ print(test_str.rjust(10, '!'))
 print(test_str.zfill(10))
 print(test_str.zfill(3))
 
+# 検索
 test_str = 'python-izm'
 print(test_str.startswith('python'))
 print(test_str.startswith('izm'))
@@ -43,10 +50,12 @@ print(test_str.startswith('izm'))
 print('z' in test_str)
 print('s' in test_str)
 
+# 大文字・小文字変換
 test_str = 'Python-Izm.Com'
 print(test_str.upper())
 print(test_str.lower())
 
+# 先頭の削除
 print('----------------------')
 test_str = '     python-izm.com'
 print(test_str)
@@ -54,6 +63,7 @@ test_str = test_str.lstrip()
 print(test_str)
 print('----------------------')
 
+# 末尾の削除
 test_str = 'python-izm.com     '
 print(test_str + '/')
 test_str = test_str.rstrip()
