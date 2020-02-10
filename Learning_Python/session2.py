3ed6dcab41a85e1ecd988f6cf324e47cff82ea49
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-11 01:18:24 +0900

    escape seaquense

---
+++ Learning_Python/session2.py
@@ -0,0 +1,26 @@
+# エスケープシーケンス
+
+# タブ文字
+print('タブ\t文字')
+
+# バックスラッシュ
+print('バックスラッシュ\\')
+
+# コードポイント
+code_point = ord('a')
+
+# 8進数値
+oct_code = oct(code_point)
+print(oct_code)
+print('\141')
+
+# 16進数値
+hex_code = hex(code_point)
+print(hex_code)
+print('\x61')
+
+# raw文字列
+raw_str = r'https://www.atmarkit.co.jp/ait/articles/1904/16/news013_2.html'
+print(raw_str)
+raw_str
+
