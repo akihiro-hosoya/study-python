9f8b45266171e081f217f499f5004c42ef75d57d
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-10 18:29:33 +0900

    test

--- python-introducton/lesson8-1.py
+++ python-introducton/lesson8-1.py
@@ -1,1 +1,17 @@
 # coding:utf-8
+
+# クラスのインポート
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.cidfonts import UnicordCIDFont
+import reportlab.lib.units as unit
+import reportlab.lib.pagesizes as pagesizes
+
+# フォントの登録
+pdfmetrics.registerFont(UnicodeCIDFont('HeiseiKakuGo-W5'))
+
+# pdfを作る
+pdf = canvas.Canvas('example.pdf', pafesize=pagesizes.A4)
+pdf.setFont('HeiseiKakuGo-W5', 14)
+pdf.drawString(10 * Unit.mm, 270 * unit.mm, '初めてのPDF')
+pdf.save()
