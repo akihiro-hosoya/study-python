a3a235fbea232672b519f81f33baf7f4fa60f620
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-02-23 20:44:11 +0900

    基本終了

--- Javascript_For_Beginners/app.js
+++ Javascript_For_Beginners/app.js
@@ -1,4 +1,27 @@
-const test = document.querySelector(".title");
-const changeColor = document.querySelector('.changeColor');
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
 
-text.style.color = "red";
+changeColor.addEventListener("click", function(){
+    text.classList.add('change');
+})
+
+const userList = document.querySelector(".name-list");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
+
+addListBtn.addEventListener("click", function() {
+    // Create on Li out of thin air
+    const newli = document.createElement('LI');
+    const liContent = document.createTextNode('sdf');
+    // Add the input value inside that new Li
+    newli.appendChild(liContent);
+    // Attaching the Li to the user list
+    userList.appendChild(newLi);
+});
+
+for(user of userList){
+    user.addEventListener('click', function(){
+    this.style.color - "red";
+    });
+}
+
